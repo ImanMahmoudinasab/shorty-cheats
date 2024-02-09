e79edaca518232d4c34b3f9694e0499d9ba3546a
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2024-02-09 08:14:34 -0800

    Setup tailwind and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./theme/theme.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px_+_2vmin)] text-[white];">
+        <img src={logo} className="App-logo size-60" alt="logo" />
       </header>
     </div>
   );
